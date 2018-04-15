b588f2f781a2248c4e61cbd7cc75c25f9784f3ee
Mateus <mateusf.junges@gmail.com>
2018-04-15 20:10:17 -0300

    Multiplexador

---
+++ mux.vhd
@@ -0,0 +1,23 @@
+------------------------------------------------------
+-- Componente MULTIPLEXADOR
+--
+-- Recebe duas entradas e ecolhe entre elas
+------------------------------------------------------
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity mux is 
+	generic (n: natural:= 1); -- numero de bits nas escolhas
+	port (
+		x,y: in std_logic_vector(n-1 downto 0);
+		s: in std_logic;
+		z: out std_logic_vector(n-1 downto 0)
+	);
+end mux;
+
+architecture beh of mux is
+	begin
+	z <= x when (s='0') else y;
+end beh;
