a21f2a0343389fa68f90adf5182bace3ee0d6419
Mateus <mateusf.junges@gmail.com>
2018-04-15 20:14:46 -0300

    Bloco de registradores

---
+++ registers.vhd
@@ -0,0 +1,73 @@
+------------------------------------------------------
+-- Bloco de registradores
+-- 
+-- Contém todos os registradores.
+------------------------------------------------------
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity registers is 
+	port (
+        ck: in std_logic;
+		reg_write: in std_logic;
+		read_reg_1, read_reg_2, write_reg: in std_logic_vector(4 downto 0);
+		write_data: in std_logic_vector(31 downto 0);
+		read_data_1, read_data_2: out std_logic_vector(31 downto 0)
+	);
+end registers;
+
+architecture beh of registers is
+
+    -- registrador de memoria de 128 byte (32 linhas * 4 bytes/linha)
+    type mem_array is array(0 to 31) of STD_LOGIC_VECTOR (31 downto 0);
+    signal reg_mem: mem_array := (
+        "00000000000000000000000000000000", -- $zero
+        "00000000000000000000000000000000", -- mem 1
+        "00000000000000000000000000000000",
+        "00000000000000000000000000000000",
+        "00000000000000000000000000000000",
+        "00000000000000000000000000000000",
+        "00000000000000000000000000000000",
+        "00000000000000000000000000000000",
+        "00000000000000000000000000000000", 
+        "00000000000000000000000000000000",
+        "00000000000000000000000000000000", -- mem 10 
+        "00000000000000000000000000000000", 
+        "00000000000000000000000000000000",
+        "00000000000000000000000000000000",
+        "00000000000000000000000000000000",
+        "00000000000000000000000000000000",
+        "00000000000000000000000000000000",
+        "00000000000000000000000000000000",
+        "00000000000000000000000000000000",
+        "00000000000000000000000000000000",  
+        "00000000000000000000000000000000", -- mem 20
+        "00000000000000000000000000000000",
+        "00000000000000000000000000000000",
+        "00000000000000000000000000000000",
+        "00000000000000000000000000000000", 
+        "00000000000000000000000000000000",
+        "00000000000000000000000000000000",
+        "00000000000000000000000000000000",
+        "00000000000000000000000000000000",
+        "00000000000000000000000000000000", 
+        "00000000000000000000000000000000", -- mem 30
+        "00000000000000000000000000000000"
+    );
+
+	begin
+
+    read_data_1 <= reg_mem(to_integer(unsigned(read_reg_1)));
+    read_data_2 <= reg_mem(to_integer(unsigned(read_reg_2)));
+
+    process(ck)
+        begin
+        if ck='0' and ck'event and reg_write='1' then
+            -- escreve no registrador da memoria quando o reg_write esta setado e em um ciclo de clock de descida
+            reg_mem(to_integer(unsigned(write_reg))) <= write_data;
+        end if;
+    end process;
+
+end beh;
